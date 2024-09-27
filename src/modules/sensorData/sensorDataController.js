40ea0600dfdce8d50ae4b934075cdf6584d33869
eprevitale <eprevitale@gmail.com>
2024-09-26 21:48:52 -0300

    Created basic routing for the sensor data

--- src/modules/sensorData/sensorDataController.js
+++ src/modules/sensorData/sensorDataController.js
@@ -1,7 +1,11 @@
-const getAllSensorData = (req, res) => {
+// GET
+const getAllSensorData = async(req, res) => {
     // req
     const body = req.body;
-    print(body);
+    if(body) {
+        console.log(body);
+    }
+    console.log("received a GET!")
 
     // res
     const json = JSON.stringify({
@@ -11,6 +15,24 @@ const getAllSensorData = (req, res) => {
     res.status(200).send(json);
 }
 
+
+// POST
+const postSensorData = async(req, res) => {
+    const { h, i, t, l, f, v } = req.body; // timestamp, sensorID, temperature, level, flow rate, volume
+    if(!h || !i || !t || !l || !f || !v) {
+        var resStatus = 400;
+        var json = { "message": "Insuficient parameters in the body. Please review the request." };
+    } else {
+        var resStatus = 201;
+    }
+    res.status(resStatus).send(json);
+}
+
+
+// --------------------------------------------
+// Exports
+// --------------------------------------------
 export default {
-    getAllSensorData
+    getAllSensorData,
+    postSensorData
 }
--- src/modules/sensorData/sensorDataRoutesV1.js
+++ src/modules/sensorData/sensorDataRoutesV1.js
@@ -4,5 +4,6 @@ import sensorDataController from './sensorDataController.js';
 const router = Router();
 
 router.get('', sensorDataController.getAllSensorData);
+router.post('', sensorDataController.postSensorData);
 
 export default router;
