e1076b6fb844075ee4860597b5e3eb2bd2096603
Eduardo Previtale <101219438+eprevitale@users.noreply.github.com>
2024-09-26 21:51:05 -0300

    Delete src/modules/sensorData/sensorDataRoutesV1.js

--- src/modules/sensorData/sensorDataRoutesV1.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import sensorDataController from './sensorDataController.js';
-
-const router = Router();
-
-router.get('', sensorDataController.getAllSensorData);
-router.post('', sensorDataController.postSensorData);
-
-export default router;
