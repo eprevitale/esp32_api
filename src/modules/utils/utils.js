465503b77bb51e4c724f001ef9fd357c827eb3fb
eprevitale <eprevitale@gmail.com>
2024-10-21 21:43:56 -0300

    Created functions to validate names and emails using regex

--- src/modules/utils/utils.js
+++ src/modules/utils/utils.js
@@ -9,3 +9,15 @@ export const isMongoObjectId = (value) => {
     const _id = typeof value !== 'string' ? value.toString() : value;
     return _id && _id.length === 24 && mongoose.Types.ObjectId.isValid(_id);
 }
+
+
+export const isValidName = (name) => {
+    let regex = /^[A-Za-zÀ-ÖØ-öø-ÿ]+(?: [A-Za-zÀ-ÖØ-öø-ÿ]+)*$/;
+    return regex.test(name);
+}
+
+
+export const isValidEmail = (email) => {
+    let regex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}(?:\.[a-z]{2,}){0,2}$/i;
+    return regex.test(email);
+}
